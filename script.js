1fb0cc85afe0f4916539d2e6d7dabac9e5f323e6
Massiel50 <massielescano@yahoo.com>
2019-11-29 19:33:59 -0500

    outlined comment user prompt to select array char data

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// 4 arrays holding user choices for characters of password gen
+var numPass = ["0","1","2","3","4","5","6","7","8","9"];
+var specialPas = ["~", "`", "!", "#", "$", "%", "^", "&", "*", "+", "=", "-", "[", "]", "'", ";", "/", "{", "}", "|", ":", "<", ">", "?"];
+var lowerCharPas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCharPas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// array composed of what char user wants in password... AKA mysteryMeat
+var mysteryMeat = [];
+
+function generatePassword()
+{
+  // Welcome user and ask for name
+  alert("Hi there! Let's make a nifty and secure password for you");
+  var userName = prompt("To get started, what is your name?");
+  // ask user if want to use numbers and push into mysteryMeat
+
+  // ask user if want to use special characters and push into mysteryMeat
+
+  // ask user if want to use lower case letters and push into mysteryMeat
+
+  // ask user if want to use upper case letters and push into mysteryMeat
+
+}
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
